b90072b54fdc8e38cd552d230f1db2b4505a3cb7
Antti Kauppila <antti.kauppila@arm.com>
2021-06-21 12:16:14 +0300

    CMake improvements for tests

--- repositories.cmake
+++ repositories.cmake
@@ -1,4 +1,12 @@
 
+option(use_https "Use https" OFF)
+
+if (use_https)
+    set(nanostack_libservice_repo       https://github.com/PelionIoT/nanostack-libservice.git)
+else()
+    set(nanostack_libservice_repo       git@github.com:PelionIoT/nanostack-libservice.git)
+endif()
+
 #Googletest for testing
 include(FetchContent)
 FetchContent_Declare(
@@ -9,3 +17,12 @@ FetchContent_Declare(
 # For Windows: Prevent overriding the parent project's compiler/linker settings
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 FetchContent_MakeAvailable(googletest)
+
+FetchContent_Declare(nanostack_libservice_decl
+    GIT_REPOSITORY      ${nanostack_libservice_repo}
+    GIT_TAG             "cmake_refactor"
+)
+
+if (${CMAKE_PROJECT_NAME} EQUAL mbedTrace)
+    FetchContent_MakeAvailable(nanostack_libservice_decl)
+endif()
